d58401f7c9e13fd6f0291e4974fcfce693883ec6
Marko_TON <markotoncanovic@gmail.com>
2022-08-02 11:20:12 +0200

    comment for getters

--- vue-composition-api-basics/src/stores/counter.js
+++ vue-composition-api-basics/src/stores/counter.js
@@ -5,6 +5,7 @@ export const useCounterStore = defineStore({
   state: () => ({
     counter: 0
   }),
+  // like computed propertis
   getters: {
     doubleCount: (state) => state.counter * 2
   },
